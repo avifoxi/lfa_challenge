8e173c8452fc41ebc75ccf41795bb567065eb40e
AviFoxi <frosenox@gmail.com>
2015-02-06 11:24:10 -0500

    improve formatting w bootstrap

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -10,22 +10,12 @@ catalog = File.read("./Library\ Display/catalog.txt")
 json_catalog = JSON.parse(catalog)
 
 names = json_catalog.flat_map{|k,v| json_catalog[k]['doc']['name']}.uniq
-
 authors = json_catalog.flat_map{|k,v| json_catalog[k]['doc']['authors']}.uniq
-
 tags = json_catalog.flat_map{|k,v| json_catalog[k]['doc']['tags']}.uniq
-
 subjects = json_catalog.flat_map{|k,v| json_catalog[k]['doc']['subjects']}.uniq
-
 languages = json_catalog.flat_map{|k,v| json_catalog[k]['doc']['languages']}.uniq
 
 get '/' do 
-
-  @tags = tags
-	@subjects = subjects
-	@languages = languages
-	@authors = authors
-
   erb :index 
 end
 
@@ -35,13 +25,13 @@ get '/books' do
   json_catalog.to_json
 end
 
-# get '/search_params' do 
-# 	content_type :json
-# 	{
-# 		names: names
-# 		authors: authors
-# 		tags: tags
-# 		subjects: subjects
-# 		languages: languages
-# 	}.to_json
-# end
+get '/search_params' do 
+	content_type :json
+	{
+		names: names,
+		authors: authors,
+		tags: tags,
+		subjects: subjects,
+		languages: languages
+	}.to_json
+end
