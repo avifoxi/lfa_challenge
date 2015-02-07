50d63b6beb1a68d58d2c0237b8e6e0e0ae6da22b
AviFoxi <frosenox@gmail.com>
2015-02-07 09:43:54 -0500

    basic filtering, fails for authors and titles. why? ;

--- public/app/js/filters.js
+++ public/app/js/filters.js
@@ -8,13 +8,50 @@ console.log('loaded');
   app.filter('bookQuery', ['$rootScope', 'userQ', function ($rootScope, userQ) {
 	  // function that's invoked each time Angular runs $digest()
 	  // pass in `item` which is the single Object we'll manipulate
-	  return function (item) {
+	  return function (books) {
 	    // return the current `item`, but call `toUpperCase()` on it
-	    $rootScope;
-	    userQ;
-	    return item;
+	  	
+	  	var q = userQ.categorySearchables;
+	  	
+	  	$rootScope.$on('userQ:updated', function (event, data) {
+		    q = userQ.categorySearchables;
+		    console.log('updated');
+		  });
+
+	  	if ( _.isEmpty(_.flatten( _.values(q) ) ) ){
+	  		return books;
+	  	} 
+	  	var booksToShow = [];
+	  	var keys = _.keys(q);
+
+	  	_.each(books, function(book){
+	      
+	  		_.each(keys, function(key){
+	  			var vals = q[key];
+	  			_.each(vals, function(val){
+	  				if (_.contains( book.doc[key], val  )){
+	  					booksToShow.push(book);
+	  				}
+	  			})
+	  		})	      
+	    });
+	    return booksToShow;
+	  	// _.each(keys, function(key){
+	  		
+
+	  	// 	console.log(key);
+
+	  	// });  
+
+	    // $rootScope;
+	    // userQ;
+	    // return item;
 	  };
 	}]);
+
+
+
+
 
 
   // app.filter('qSelection', ['$rootScope', function($rootScope) {
