7c6fcbb0d9340f241647e3b8081de0f70b61055b
AviFoxi <frosenox@gmail.com>
2015-02-06 19:56:50 -0500

    finally, custom service is registered and availabl in BooksCtrl

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,12 @@
 (function() {
-  var app = angular.module('libFA', ['lfa-directives']);
+  var app = angular.module('libFA', ['lfa-directives', 'queryServices']);
 
-  app.controller('BooksCtrl', ['$http', function($http){
+  app.controller('BooksCtrl', ['$http', 'userQ', function($http, userQ){
     var library = this;
     library.books = [];
-    
+    library.query = userQ.categoryVals;
+
+
     $http.get('/books').success(function(books) {
       library.books = books;
       // console.log(books);
---
+++ public/app/js/services.js
@@ -0,0 +1,12 @@
+(function() {
+  
+  var app = angular.module('queryServices', []);
+
+  app.service('userQ', function UserQ() {
+  	var uq = this;
+  	uq.categoryVals = {};
+	});
+
+})();
+
+
