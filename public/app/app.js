425acfdcb0d3f5fd1a16cde82962ee90eda60598
AviFoxi <frosenox@gmail.com>
2015-02-07 20:15:58 -0500

    manually merge services refactor and ui-router addition... now have a semi functional routing app

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,33 @@
 (function() {
-  var app = angular.module('libFA', ['lfa-directives', 'queryServices', 'userQfilters', 'booksServices']);
+  var app = angular.module('libFA', ['lfa-directives', 'queryServices', 'userQfilters', 'booksServices', 'ui.router']);
+
+  
+  app.config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+     .state('home', {
+         url: '/home',
+         templateUrl: 'app/views/home.html'
+     })
+     .state('books', {
+         url: '/books',
+         templateUrl: 'app/views/books.html',
+         controller: 'BooksCtrl'
+     })
+     // .state('books.page', {
+     //     url: '/:id',
+     //     templateUrl: 'app/views/books.page.html'
+     //     // ,
+     //     // controller: function($scope, $stateParams) {
+     //     //     // $stateParams passes in item in url
+     //     //   $scope.book = $stateParams.book;
+     //     // }
+     // });
+   $urlRouterProvider.otherwise('/home');
+ })
+
+
+
+
 
   app.controller('BooksCtrl', ['$http', 'librarian', '$rootScope', function($http, librarian, $rootScope){
     var library = this;
